54b1baba57b7ea5b97150579eac516d127ff8c3b
Roger Faco <rfacodev@gmail.com>
2021-03-26 01:08:51 -0300

    Fixing code + comment process

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
 
-
-const todos = JSON.parse(localStorage.getItem('todos'))
+const todos = JSON.parse(localStorage.getItem('todos')) //Criando Local Storage
 
 if(todos) {
   todos.forEach(todo => addTodo(todo))
 }
-
-
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -31,28 +28,24 @@ function addTodo(todo) {
 
     todoEl.innerText = todoText
 
-    todoEl.addEventListener('click', ()  => 
-    {
+    todoEl.addEventListener('click', ()  => {
     todoEl.classList.toggle('completed')
     updateLS()
     })
 
-    todoEl.addEventListener('contextmenu', (e) => {
-    e.preventDefault()
-
+    todoEl.addEventListener('contextmenu', (e) => { e.preventDefault()
     todoEl.remove()
     updateLS()
-    } )
+    })
 
     todosUL.appendChild(todoEl)
-
-    input.value = ''
+    input.value = '' //Limpar valor
 
     updateLS()
-
   }
 }
 
+/* Update Local Storage */
 function updateLS() {
   todosEl = document.querySelectorAll('li')
   const todos = []
